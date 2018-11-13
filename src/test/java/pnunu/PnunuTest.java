3f175ad086f42edd74c71f8201fa8c05f4076d7d
pnunu <398269786@qq.com>
2018-11-13 23:25:22 +0800

    测试

---
+++ src/test/java/pnunu/PnunuTest.java
@@ -0,0 +1,16 @@
+package pnunu;
+
+import org.junit.Test;
+
+/**
+ * @author: pnunu
+ * @create: 2018-11-13 23:23
+ */
+public class PnunuTest {
+
+
+    @Test
+    public void test() {
+        System.out.println("00000000");
+    }
+}
